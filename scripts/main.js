453e39e9bdfe0179afec316ca7be7fab7c0ebd02
Milana Novohrost <milashka222005@gmail.com>
2023-04-04 18:25:19 +0300

    cards question done

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ const request = new Request(URL, options);
 fetch(request)
   .then((res) => res.json())
   .then((data) => {
-    cards.push(data);
+    cards.splice(0, cards.length, ...data);
     localStorage.setItem("cards", JSON.stringify(data));
     renderCards();
   });
@@ -24,20 +24,16 @@ window.onload = () => {
   renderCards();
 };
 
-let cardsList = document.querySelector(".cards__list");
+const cardsList = document.querySelector(".cards__list");
 
-let card = `<div class="cards__item"><div class="cards__item-img"><img src="" alt="image" /><button class="cards__item-view">Быстрый просмотр</button></div><div class="cards__item-prices"><p class="cards__item-currPrice"></p><p class="cards__item-oldPrice"></p></div><p class="cards__item-text"></p><div class="cards__item-group"><button class="cards__item-addToCart">В корзину</button><div class="cards__item-btns btns"><button class="btns__minus">-</button><p class="btns-number"></p><button class="btns__plus">+</button></div></div></div>`;
+const getCardHtml = (data) => `<div class="cards__item"><div class="cards__item-img"><img src="${data.image}" alt="image" /><button class="cards__item-view">Быстрый просмотр</button></div><div class="cards__item-prices"><p class="cards__item-currPrice"></p><p class="cards__item-oldPrice"></p></div><p class="cards__item-text"></p><div class="cards__item-group"><button class="cards__item-addToCart">В корзину</button><div class="cards__item-btns btns"><button class="btns__minus">-</button><p class="btns-number"></p><button class="btns__plus">+</button></div></div></div>`;
 
 const renderCards = () => {
   cardsList.innerHTML = "";
-  cards.forEach((el) => (cardsList.innerHTML += card));
+  cards.forEach((el) => (cardsList.innerHTML += getCardHtml(el)));
   localStorage.setItem("cards", JSON.stringify(cards));
 };
 
-const getCards = JSON.parse(localStorage.getItem('cards'));
-getCards.forEach((elem) => {
-  //  `<div class="cards__item"><div class="cards__item-img"><img src="${elem.image}" alt="image" /><button class="cards__item-view">Быстрый просмотр</button></div><div class="cards__item-prices"><p class="cards__item-currPrice"></p><p class="cards__item-oldPrice"></p></div><p class="cards__item-text"></p><div class="cards__item-group"><button class="cards__item-addToCart">В корзину</button><div class="cards__item-btns btns"><button class="btns__minus">-</button><p class="btns-number"></p><button class="btns__plus">+</button></div></div></div>`;
-   
-})
-// console.log(getCards);
-//*****
+
+
+
