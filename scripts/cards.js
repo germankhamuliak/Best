c8c4817d0cf6f1c5cc9ad1486fb3ef0d7806cc8b
Milana <milashka222005@gmail.com>
2023-04-10 18:27:52 +0300

    беды с корзиной

--- scripts/cards.js
+++ scripts/cards.js
@@ -32,7 +32,7 @@ const getCardHtml = (data) =>
       <a href="#" class="cards__item-view">Быстрый просмотр</a>
     </div>
     <p class="cards__item-price">${data.price} ${currency.innerHTML}</p>
-    <p class="cards__item-text">${data.description}</p>
+    <p class="cards__item-text">${data.title} ~ ${data.description}</p>
     <div class="cards__item-group">
       <button class="cards__item-addToCart">В корзину</button>
       <div class="cards__item-btns btns">
@@ -42,6 +42,8 @@ const getCardHtml = (data) =>
       </div>
     </div>
 </div>`;
+
+//корзина и карточки
 
 const cart = JSON.parse(localStorage.getItem("Cart")) || [];
 
@@ -58,23 +60,48 @@ const renderCards = () => {
   });
 };
 
-//корзина
+const divCartHTML = () => `<div class="basket">
+  <div class="basket__list">
+  </div>
+</div>
+`;
+const basketList = document.querySelector(".basket__list");
+
+const getCartItem = (el) => `<div class="basket__item">
+    <div class="basket__image">
+    <img src="${el.image}" alt="image">
+    </div>
+    <div class="basket__text">
+    // <h3 class="basket__text-title"></h3>
+    <p class="basket__text-description">${el.description}</p>
+    <p class="basket__text-price">${el.price} ${currency.innerHTML}</p>
+    </div>
+    </div>`;
 
 const cartHTML = document.querySelector(".header__cart");
-cartHTML.addEventListener("click", () => {});
+cartHTML.addEventListener("click", () => {
+  divCartHTML();
+  basketList.innerHTML = "";
+  JSON.parse(localStorage.getItem("Cart"));
+  if (cart && cart.length > 0) {
+    cart.forEach((el) => {
+      basketList.innerHTML += getCartItem(el);
+    });
+  }
+});
 
-const getBasketHTML = (data) =>
-  `<div class="basket">
-    <div class="basket__list">
-        <div class="basket__item">
-            <div class="basket__image">
-              <img src="${data.image}" alt="image">
-            </div>
-            <div class="basket__text">
-                // <h3 class="basket__text-title"></h3>
-                <p class="basket__text-description">${data.description}</p>
-                <p class="basket__text-price">${data.price} ${currency.innerHTML}</p>
-            </div>
-        </div>
-    </div>
-</div>`;
+// const getBasketHTML = (data) =>
+//   `<div class="basket">
+//     <div class="basket__list">
+//         <div class="basket__item">
+//             <div class="basket__image">
+//               <img src="${data.image}" alt="image">
+//             </div>
+//             <div class="basket__text">
+//                 // <h3 class="basket__text-title"></h3>
+//                 <p class="basket__text-description">${data.description}</p>
+//                 <p class="basket__text-price">${data.price} ${currency.innerHTML}</p>
+//             </div>
+//         </div>
+//     </div>
+// </div>`;
