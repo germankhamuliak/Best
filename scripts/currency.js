2af4366db13443ebbdac17893c8ec637fba25e2b
German <germanbolk608@gmail.com>
2023-04-18 23:21:34 +0300

    цена работает

--- scripts/currency.js
+++ scripts/currency.js
@@ -1,6 +1,17 @@
 const currencyBtn = document.querySelector('.header__currency')
 const currencyList = document.querySelector('.currency-list')
+const currency = document.querySelector('.currency')
+const currencyItem = document.querySelectorAll('.currency-item')
 
 currencyBtn.addEventListener('click', ()=>{
     currencyList.classList.toggle('active')
+})
+
+
+currencyItem.forEach((el)=>{
+    el.addEventListener('click',()=>{
+        currency.innerHTML = el.innerHTML;
+        location.reload();
+        
+    })
 })
