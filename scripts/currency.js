dd5ec46549adf689943bca1c8e4347e75a56b6ae
German <germanbolk608@gmail.com>
2023-04-19 13:18:41 +0300

    цена конечная версия

--- scripts/currency.js
+++ scripts/currency.js
@@ -3,14 +3,26 @@ const currencyList = document.querySelector('.currency-list')
 const currency = document.querySelector('.currency')
 const currencyItem = document.querySelectorAll('.currency-item')
 
+
+
+
 currencyBtn.addEventListener('click', ()=>{
     currencyList.classList.toggle('active')
 })
 
 
+document.addEventListener('DOMContentLoaded',()=>{
+    if(localStorage.getItem('currency')){
+        currency.innerHTML = localStorage.getItem('currency')
+    }else{
+    currency.innerHTML = `<img class="flag-icon header__flag" src="/RB.5f7f00e2.jpg" alt="rb" />
+    <span class="currency-name">BYN</span>`;
+    }
+})
+
 currencyItem.forEach((el)=>{
     el.addEventListener('click',()=>{
         currency.innerHTML = el.innerHTML;
-        
+        localStorage.setItem('currency', currency.innerHTML);
     })
 })
