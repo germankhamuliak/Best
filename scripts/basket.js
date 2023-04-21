1e2d65e5d8496dfeae64431d0651bed927f16759
German <germanbolk608@gmail.com>
2023-04-21 23:28:04 +0300

    Решил проблему с корзиной!

--- scripts/basket.js
+++ scripts/basket.js
@@ -41,21 +41,15 @@ basketList.addEventListener("click", ({target}) => {
   if (target.classList.contains("basket__delete")) {
     const itemDel = cart.find((elem) => {
       if(target.closest(".basket__item").id ===  elem.id){
-        console.log( elem.id, target.closest(".basket__item").id )
         return elem;
       }
-      
     });
-    console.log(itemDel);
     const itemDelId = cart.indexOf(itemDel);
     cart.splice(itemDelId, 1);
     localStorage.setItem("Cart", JSON.stringify(cart));
     renderBasket(); 
     headCartNumber.innerHTML = cart.length;
-    localStorage.setItem(
-      "NumberOfGoods",
-      JSON.stringify(headCartNumber.innerHTML)
-    );
+    localStorage.setItem("NumberOfGoods",JSON.stringify(headCartNumber.innerHTML));
   }
 });
 
