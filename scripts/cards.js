86195d6adc38c14b73304407ffe36d64a1f863cb
Milana <milashka222005@gmail.com>
2023-04-25 19:30:26 +0300

    + - для всех

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,5 +1,5 @@
-import { cart,currency,headCartNumber,headCartAmount } from "./basket.js";
-export {cardsList};
+import { cart, currency, headCartNumber, headCartAmount } from "./basket.js";
+export { cardsList };
 
 const URL = "https://6428388446fd35eb7c4e2663.mockapi.io/wild/pr/card";
 
@@ -37,14 +37,10 @@ const renderCards = () => {
         headCartAmount.classList.add("header__cart-amount_active");
       }
       alert("Товар добавлен в корзину");
-      el.classList.add('not-active')
+      el.classList.add("not-active");
     });
   });
 };
-
-const minus = document.querySelectorAll(".btns__minus");
-const numberGoods = document.querySelectorAll(".btns__number");
-const plus = document.querySelectorAll(".btns__plus");
 
 window.onload = () => {
   const prevCards = JSON.parse(localStorage.getItem("cards"));
@@ -69,99 +65,3 @@ const getCardHtml = (data) =>
       <button class="cards__item-addToCart">В корзину</button>
       <button class="cards__item-inCart">Добавлено в корзину</button>   
 </div>`;
-
-// numberGoods.forEach((el) => {
-//   // el.innerHTML = Number(el.innerHTML);
-//   el.innerHTML = 0;
-//   el.innerHTML >= 0;
-// });
-
-// minus.forEach((el) => {
-//   el.addEventListener("click", () => {
-//     numberGoods.forEach((elem, id) => {
-//       elem.innerHTML -= 1;
-
-//       console.log(id);
-//     });
-//     // headCartAmount.innerHTML -= 1;
-//   });
-// });
-
-// plus.forEach((el) => {
-//   el.addEventListener("click", () => {
-//     numberGoods.forEach((elem, id) => {
-//       elem.innerHTML += 1;
-
-//       console.log(id);
-//     });
-//     // headCartAmount.innerHTML += 1;
-//   });
-// });
-
-//корзина и отображение карточек
-
-// const basket = document.querySelector(".basket");
-// const basketContainer = document.querySelector(".basket__container");
-// const cart = JSON.parse(localStorage.getItem("Cart")) || [];
-// const basketList = document.querySelector(".basket__list");
-
-// const getCartItem = (el) => `<div class="basket__item" id="${el.id}">
-//     <div class="basket__image">
-//       <img src="${el.image}" alt="image">
-//     </div>
-//     <div class="basket__text">
-//       <h3 class="basket__text-title">${el.title}</h3>
-//       <p class="basket__text-description">${el.description}</p>
-//       <p class="basket__text-price">${el.price} ${currency()}</p>
-//       <div class="btns">
-//         <button class="btns__minus">-</button>
-//         <p class="btns__number">0</p>
-//         <button class="btns__plus">+</button>
-//       </div>
-//       <button class="basket__buy">Купить</button><br>
-//       <button class="basket__delete" id="${el.id}">Удалить</button>
-//     </div>
-//   </div>`;
-
-// basketList.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("basket__delete")) {
-//     const itemDel = cart.find((elem) => {
-//       elem.id == e.target.id;
-//     });
-//     itemDelId = cart.indexOf(itemDel);
-//     console.log(itemDel);
-//     cart.splice(itemDelId, 1);
-//     localStorage.setItem("Cart", JSON.stringify(cart));
-//     renderBasket(); //отрисовка элемента корзины
-//     headCartNumber.innerHTML = cart.length;
-//     localStorage.setItem(
-//       "NumberOfGoods",
-//       JSON.stringify(headCartNumber.innerHTML)
-//     );
-//   }
-// });
-
-// renderBasket = () => {
-//   basketList.innerHTML = "";
-//   if (cart && cart.length > 0) {
-//     cart.forEach((el) => {
-//       basketList.innerHTML += getCartItem(el);
-//     });
-//   }
-// };
-
-// const bodyCart = document.querySelector(".body");
-// const cartHTML = document.querySelector(".header__cart");
-// cartHTML.addEventListener("click", () => {
-//   renderBasket();
-//   basketContainer.classList.add("basket__active");
-//   bodyCart.classList.add("body-modal");
-//   // header.classList.add("header-modal");
-// });
-
-// const basketClose = document.querySelector(".basket__close");
-// basketClose.addEventListener("click", () => {
-//   basketContainer.classList.remove("basket__active");
-//   bodyCart.classList.remove("body-modal");
-//   // header.classList.remove("header-modal");
-// });
