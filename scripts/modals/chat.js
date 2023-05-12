8fb9ad072ce2daca086b852e314b11952627843d
danya <danya.avdey@mail.ru>
2023-05-12 19:03:03 +0300

    chat

--- scripts/modals/chat.js
+++ scripts/modals/chat.js
@@ -2,6 +2,16 @@ const chatIcon = document.querySelector('.chat__img-conteiner')
 const problem = document.querySelector('.header__massege-abote-problems')
 const chatWindow = document.querySelector('.chat__window')
 const chatClose = document.querySelector('.chat__close')
+const chatBody = document.querySelector('.chat__body')
+const chatInput = document.querySelector('.chat__input')
+const chatButton = document.querySelector('.chat__button')
+let question;
+const chatQuestion =() => `<div class="chat__question">
+                        <span class="name">Вы</span>
+                        <span class="message">${question}</span>
+                    </div>`
+
+
 
 chatIcon.addEventListener('click', () =>{
     chatWindow.classList.add('chat-active')
@@ -15,3 +25,15 @@ chatClose.addEventListener('click', (e) =>{
     e.preventDefault()
     chatWindow.classList.remove('chat-active')
 })
+
+
+chatButton.addEventListener('click', () => {
+    question = chatInput.value;
+    chatInput.value = "";
+    chatBody.append(chatQuestion())
+    question = "";
+})
+
+
+
+
