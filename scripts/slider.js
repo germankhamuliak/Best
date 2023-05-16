c716ee7a879d7391d0a0a88f92a11d5ce47dc162
German <germanbolk608@gmail.com>
2023-05-16 13:21:33 +0300

    pretter правки

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,61 +1,56 @@
-const sliderImages = document.querySelectorAll('.slider__img'),
-sliderLine = document.querySelector('.slider__line'),
-sliderDots = document.querySelectorAll('.slider__dot'),
-sliderBtnNext = document.querySelector('.slider__btn-next')
-sliderBtnPrev = document.querySelector('.slider__btn-prev');
-
+const sliderImages = document.querySelectorAll(".slider__img"),
+      sliderLine = document.querySelector(".slider__line"),
+      sliderDots = document.querySelectorAll(".slider__dot"),
+      sliderBtnNext = document.querySelector(".slider__btn-next"),
+      sliderBtnPrev = document.querySelector(".slider__btn-prev");
 
 let sliderCount = 0,
     sliderWidth;
 
+window.addEventListener("resize", showSlide);
 
-window.addEventListener('resize', showSlide)
+sliderBtnNext.addEventListener("click", nextSlide);
+sliderBtnPrev.addEventListener("click", prevSlide);
 
-
-sliderBtnNext.addEventListener('click', nextSlide)
-sliderBtnPrev.addEventListener('click',prevSlide)
-
-
-function showSlide(){
-    sliderWidth = document.querySelector('.slider').offsetWidth
-    sliderLine.style.width = sliderWidth*sliderImages.length + 'px';
-    sliderImages.forEach(item => item.style.width = sliderWidth + 'px');
-    rollSlider()
+function showSlide() {
+  sliderWidth = document.querySelector(".slider").offsetWidth;
+  sliderLine.style.width = sliderWidth * sliderImages.length + "px";
+  sliderImages.forEach((item) => (item.style.width = sliderWidth + "px"));
+  rollSlider();
 }
-showSlide()
+showSlide();
 
-function nextSlide(){
-    sliderCount++;
-    if (sliderCount >= sliderImages.length)sliderCount=0;
+function nextSlide() {
+  sliderCount++;
+  if (sliderCount >= sliderImages.length) sliderCount = 0;
+  rollSlider();
+  thisSlide(sliderCount);
+}
+
+function prevSlide() {
+  sliderCount--;
+  if (sliderCount < 0) sliderCount = sliderImages.length - 1;
+  rollSlider();
+  thisSlide(sliderCount);
+}
+
+function rollSlider() {
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+  sliderDots.forEach((item) => item.classList.remove("active-dot"));
+  sliderDots[index].classList.add("active-dot");
+}
+
+sliderDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    sliderCount = index;
     rollSlider();
-    thisSlide(sliderCount)
-}
-
-function prevSlide(){
-    sliderCount--;
-    if(sliderCount<0) sliderCount = sliderImages.length-1;
-    rollSlider();
-    thisSlide(sliderCount)
-}
-
-function rollSlider(){
-    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
-}
-
-function thisSlide(index){
-    sliderDots.forEach(item => item.classList.remove('active-dot'));
-    sliderDots[index].classList.add('active-dot')
-}
-
-sliderDots.forEach((dot,index) =>{
-    dot.addEventListener('click', () => {
-        sliderCount = index;
-        rollSlider();
-        thisSlide(sliderCount);
-    })
-})
-
+    thisSlide(sliderCount);
+  });
+});
 
 setInterval(() => {
-    nextSlide()
-}, 10000)
+  nextSlide();
+}, 10000);
--- scripts/varibles.js
+++ scripts/varibles.js
@@ -1,13 +1,11 @@
-const byn = 'BYN';
-const rub = 'RUB';
-const usd = 'USD';
+const byn = "BYN";
+const rub = "RUB";
+const usd = "USD";
 
-const defaultCurrency =`<img class="flag-icon currency__flag" src="/RB.5f7f00e2.jpg" alt="rb" /><span class="currency__name val">BYN</span>`;
+const defaultCurrency = `<img class="flag-icon currency__flag" src="/RB.5f7f00e2.jpg" alt="rb" /><span class="currency__name val">BYN</span>`;
 
 const URL = "https://6428388446fd35eb7c4e2663.mockapi.io/wild/pr/card";
 
-const options = {method: "GET"};
+const options = { method: "GET" };
 
-
-export {byn, rub, usd, defaultCurrency, URL, options };
-
+export { byn, rub, usd, defaultCurrency, URL, options };
