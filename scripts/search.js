cf6cf148d57d0cfc95b606e69e63affd94a9310c
Milana Novohrost <milashka222005@gmail.com>
2023-05-16 21:29:25 +0300

    чёт не работает в поиске

--- scripts/search.js
+++ scripts/search.js
@@ -1,10 +1,22 @@
-import { cards, renderCards } from "./cards_and_basket/cards.js";
+import {
+  cards,
+  renderCards,
+  getCardDefHtml,
+  cardsList,
+} from "./cards_and_basket/cards.js";
 
 const search = document.querySelector("#search");
 const searchList = document.querySelector(".search__list");
 const searcIcon = document.querySelector(".search-icon");
 
+const searchTitles = () => {
+  cards.forEach((el) => {
+    // searchList.append(el.title);
+  });
+};
+
 search.addEventListener("click", () => {
+  searchTitles();
   searcIcon.classList.add("search-icon-active");
   searchList.classList.add("search__list-active");
 });
@@ -16,20 +28,30 @@ document.addEventListener("click", ({ target }) => {
   }
 });
 
-const searchTitles = () => {
-  const titles = cards.forEach((el) => el.title);
-  searchList.push(titles);
-};
+// search.oninput = () => {
+//   //el.textContent ??
+//   if (search.value) {
+//     cards.forEach((el) => {
+//       if (el.title !== search.value) {
+//         const ind = indexOf(el);
+//         cards.splice(ind, 1);
+//         renderCards();
+//       }
+//     });
+//   }
+// };
 
 search.oninput = () => {
-  //el.textContent ??
-  if (search.value) {
-    cards.forEach((el) => {
-      if (el.title !== search.value) {
-        const ind = indexOf(el);
-        cards.splice(ind, 1);
-        renderCards();
-      }
+  if (!search.value) {
+    searchList.classList.add("search__list-active");
+  } else if (search.value) {
+    searchList.classList.remove("search__list-active");
+    const targ = cards.filter((el) => {
+      el.title == search.value;
+      //el.textContent ??
+    });
+    targ.forEach((elem) => {
+      cardsList.innerHTML += getCardDefHtml(elem);
     });
   }
 };
