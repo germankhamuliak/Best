3d6a0812a4eeb9ab99302bf8ff62ec858e48e5d7
Milana Novohrost <milashka222005@gmail.com>
2023-05-18 22:55:09 +0300

    поиск, что-то есть

--- scripts/search.js
+++ scripts/search.js
@@ -7,51 +7,55 @@ import {
 
 const search = document.querySelector("#search");
 const searchList = document.querySelector(".search__list");
-const searcIcon = document.querySelector(".search-icon");
+const searchIcon = document.querySelector(".search-icon");
+const searchList2 = document.querySelector(".search__list_2");
+
+const buildLi = (el) => `<li class="search__link_2">${el.title}</li>`;
+
+const arrTitles = []; ///////
 
 const searchTitles = () => {
-  cards.forEach((el) => {
-    // searchList.append(el.title);
-  });
+  if (searchList2.innerHTML == 0) {
+    cards.forEach((el) => {
+      if (el.title) {
+        searchList2.innerHTML += buildLi(el);
+        arrTitles.push(el.title);  //////
+      }
+    });
+    searchList2.classList.add("search__list-active");
+  } else {
+    searchList2.classList.add("search__list-active");
+  }
 };
 
+const lettersFunc = () => {   ///////////
+  let suitableTitles = arrTitles.filter(el => {
+    el.substr(0) == search.value.substr(0);
+  });
+
+}
+
 search.addEventListener("click", () => {
-  searchTitles();
-  searcIcon.classList.add("search-icon-active");
   searchList.classList.add("search__list-active");
 });
 
 document.addEventListener("click", ({ target }) => {
   if (target != search) {
     searchList.classList.remove("search__list-active");
-    searcIcon.classList.remove("search-icon-active");
+    searchIcon.classList.remove("search-icon-active");
+    searchList2.classList.remove("search__list-active");
   }
 });
 
-// search.oninput = () => {
-//   //el.textContent ??
-//   if (search.value) {
-//     cards.forEach((el) => {
-//       if (el.title !== search.value) {
-//         const ind = indexOf(el);
-//         cards.splice(ind, 1);
-//         renderCards();
-//       }
-//     });
-//   }
-// };
-
 search.oninput = () => {
   if (!search.value) {
+    searchList2.classList.remove("search__list-active");
     searchList.classList.add("search__list-active");
+    searchIcon.classList.add("search-icon-active");
   } else if (search.value) {
     searchList.classList.remove("search__list-active");
-    const targ = cards.filter((el) => {
-      el.title == search.value;
-      //el.textContent ??
-    });
-    targ.forEach((elem) => {
-      cardsList.innerHTML += getCardDefHtml(elem);
-    });
+    searchTitles();
+
+    lettersFunc();   /////
   }
 };
