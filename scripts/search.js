96135ac1fdb93715fd7015f09c13f610c5ae1771
German <germanbolk608@gmail.com>
2023-05-19 00:31:11 +0300

    Поиск почти сделан

--- scripts/search.js
+++ scripts/search.js
@@ -1,42 +1,51 @@
-import {
-  cards,
-  renderCards,
-  getCardDefHtml,
-  cardsList,
-} from "./cards_and_basket/cards.js";
+import { cards } from "./cards_and_basket/cards.js";
 
 const search = document.querySelector("#search");
 const searchList = document.querySelector(".search__list");
 const searchIcon = document.querySelector(".search-icon");
-const searchList2 = document.querySelector(".search__list_2");
+const searchList2 = document.querySelector(".search__list-2");
 
-const buildLi = (el) => `<li class="search__link_2">${el.title}</li>`;
+const buildLi = (el) => `<li class="search__link">${el.title}</li>`;
 
-const arrTitles = []; ///////
+  document.addEventListener("click",({target})=>{
+    if(target.classList.contains('search__link'))
+    search.value = target.textContent;
+  })
 
-const searchTitles = () => {
-  if (searchList2.innerHTML == 0) {
-    cards.forEach((el) => {
-      if (el.title) {
-        searchList2.innerHTML += buildLi(el);
-        arrTitles.push(el.title);  //////
-      }
-    });
-    searchList2.classList.add("search__list-active");
-  } else {
-    searchList2.classList.add("search__list-active");
-  }
-};
 
-const lettersFunc = () => {   ///////////
-  let suitableTitles = arrTitles.filter(el => {
-    el.substr(0) == search.value.substr(0);
+
+let suitableTitles = [];  
+const lettersFunc = () => {   
+   suitableTitles = cards.filter(el => {
+    return el.title.toLowerCase().startsWith(search.value)||el.title.startsWith(search.value) 
   });
-
 }
 
+const searchTitles = () => {
+    if(suitableTitles.length==0){
+      searchList2.innerHTML = "";
+      searchList2.innerHTML += `<li class="search__link">По вашему запросу ничего не найдено</li>`;
+    }else{
+      searchList2.innerHTML = "";
+      searchList2.innerHTML += `<li class="search__link">По вашему запросу найдено</li>`;
+      suitableTitles.forEach((el) => {
+        searchList2.innerHTML += buildLi(el);  
+    });
+    searchList2.classList.add("search__list-active");
+    }
+  };
+
+
+
 search.addEventListener("click", () => {
+  if(!search.value){
   searchList.classList.add("search__list-active");
+  searchIcon.classList.add("search-icon-active");
+  }else{
+  searchList2.classList.add("search__list-active");
+  searchIcon.classList.add("search-icon-active");
+  }
+
 });
 
 document.addEventListener("click", ({ target }) => {
@@ -52,10 +61,11 @@ search.oninput = () => {
     searchList2.classList.remove("search__list-active");
     searchList.classList.add("search__list-active");
     searchIcon.classList.add("search-icon-active");
-  } else if (search.value) {
+  }  
+  if (search.value) {
     searchList.classList.remove("search__list-active");
+    searchList2.classList.add("search__list-active");
+    lettersFunc();
     searchTitles();
-
-    lettersFunc();   /////
   }
 };
